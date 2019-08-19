c5a7745ca679cf1a3a925305e01edaa47056d20a
y-wakugami <renesis2b@gmail.com>
2019-08-19 20:56:41 +0900

    Initial commet

---
+++ AretctAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AretctAdmin
+//
+//  Created by yw on 2019/08/19.
+//  Copyright © 2019 yw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
