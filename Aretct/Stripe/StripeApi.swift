3631044c7723273b9404f959f0414527f354ef1e
y-wakugami <renesis2b@gmail.com>
2019-09-17 00:11:11 +0900

    create StripeApi.swift

---
+++ Aretct/Stripe/StripeApi.swift
@@ -0,0 +1,40 @@
+//
+//  StripeApi.swift
+//  Aretct
+//
+//  Created by yw on 2019/09/16.
+//  Copyright © 2019 yw. All rights reserved.
+//
+
+import Foundation
+import Stripe
+import FirebaseFunctions
+
+let StripeApi = _StripeApi()
+
+class _StripeApi: NSObject, STPCustomerEphemeralKeyProvider {
+    
+    func createCustomerKey(withAPIVersion apiVersion: String, completion: @escaping STPJSONResponseCompletionBlock) {
+        
+        let data = [
+            "stripe_version": apiVersion,
+            "customer_id" : UserService.user.stripeId
+        ]
+        
+        Functions.functions().httpsCallable("createEphemeralKey").call(data) { (result, error) in
+            
+            if let error = error {
+                debugPrint(error.localizedDescription)
+                completion(nil, error)
+                return
+            }
+            
+            guard let key = result?.data as? [String: Any] else {
+                completion(nil, nil)
+                return
+            }
+            
+            completion(key, nil)
+        }
+    }
+}
