2b2401e22444c96497864e78c09756a56278a333
y-wakugami <renesis2b@gmail.com>
2019-09-29 15:39:10 +0900

    [change]Productsページにカテゴリー名を表示する
    refs ProductsVCにLabelの参照とSegueで受け取ったCategoryインスタンスのnameプロパティの値を代入酢ようにした

--- Aretct/Controller/ProductsVC.swift
+++ Aretct/Controller/ProductsVC.swift
@@ -16,6 +16,7 @@ class ProductsVC: UIViewController, ProductCellDelegate {
 
     // Outlets
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var categoryLabel: UILabel!
     
     
     // Variables
@@ -28,6 +29,7 @@ class ProductsVC: UIViewController, ProductCellDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        categoryLabel.text = category.name
         db = Firestore.firestore()
         
         tableView.delegate = self
